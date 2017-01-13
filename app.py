ce045a6d02e1de21259f5021d344807c97e19691
sberkani15 <slimane.berkani15@gmail.com>
2017-01-13 10:19:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,14 @@ app.secret_key = 'development key'
 def main():
    form = ScrapForm()
    
-   
+   cles=[]
    if request.method == 'POST':
         _motcle=request.form['mot_cle']
-        #_lismc=_motcle.split(" ")
+        _lismc=_motcle.split("\n")
+        for wd in _lismc:
+         tmp=wd.replace("\r","")
+         cles.append(tmp)
+         print cles
        
         
         headers = {'user-agent': 'Mozilla/5.0'}
@@ -25,40 +29,42 @@ def main():
         #profondeur de recherche
         _profondeur=int(request.form['profondeur'])
         
-        #parcourir les pages en fonction de la profondeur
-        for start in range(0,_profondeur):
-            find=None
-            url = "http://www.google.fr/search?hl=fr&q="+str(_motcle)+"&start="+str(start*10)+"&filter=0"
-            pge=requests.get(url,headers=headers)
-            #utilisation de beautifulsoup pour le parsing
-            soup = BeautifulSoup(pge.content)
-            #recuperation des liens: tous les liens sont tagues par cite
-            lien=soup.findAll('cite')
-            position=0
-            #variable find verifie si on a trouve un resultat ou pas initialement faux
-            find=None
-            #sauvegarde des liens retournes par google
-            for li in lien:
-              _links.append(li.text)
-        
-        #chercher notre site web parmi les liens resultat 
-        for li in _links:
-              position+=1
-              if request.form['url'] in li and not find:
-                res = Result(request.form['url'],li,position)
-                res.mot_cle=_motcle
-                res.url=li
-                res.position=position
-                _list.append(res)
-                find=True
-                
-        #si on a pas trouve de liens on assigne NA au resultat
-        if not find:
-                res = Result(_motcle,"NA","0")
-                res.mot_cle=_motcle
-                res.url="NA"
-                res.position="NA"
-                _list.append(res)		
+        #parcourir les pages en fonction de la profondeur pour chaque mot cle
+     for cle in cles:
+                _links[:]=[]
+		for start in range(0,_profondeur):
+		    find=None
+		    url = "http://www.google.fr/search?hl=fr&q="+str(cle)+"&start="+str(start*10)+"&filter=0"
+		    pge=requests.get(url,headers=headers)
+		    #utilisation de beautifulsoup pour le parsing
+		    soup = BeautifulSoup(pge.content)
+		    #recuperation des liens: tous les liens sont tagues par cite
+		    lien=soup.findAll('cite')
+		    position=0
+		    #variable find verifie si on a trouve un resultat ou pas initialement faux
+		    find=None
+		    #sauvegarde des liens retournes par google
+		    for li in lien:
+		      _links.append(li.text)
+		
+		#chercher notre site web parmi les liens resultat 
+		for li in _links:
+		      position+=1
+		      if request.form['url'] in li and not find:
+		        res = Result(request.form['url'],li,position)
+		        res.mot_cle=cle
+		        res.url=li
+		        res.position=position
+		        _list.append(res)
+		        find=True
+		        
+		#si on a pas trouve de liens on assigne NA au resultat
+		if not find:
+		        res = Result(_motcle,"NA","0")
+		        res.mot_cle=cle
+		        res.url="NA"
+		        res.position="NA"
+		        _list.append(res)		
   
         return render_template('scrap.html', pages=_list, form=form)
    else:
